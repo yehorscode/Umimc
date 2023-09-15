fabb0744138bea7937b1a39a7d10d59382db0d2a
Yehor <omelianenkoegor@gmail.com>
2023-09-15 15:53:23 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 import webbrowser
 import requests
 from colorama import Fore, Back, Style
-
-headers = {
-    'User-Agent': 'My User Agent 1.0',
-    'From': 'youremail@domain.example'  # This is another valid field
+import urllib.request
+headerss = {
+    'User-Agent': 'yehorscode/Umimc (umimc@proton.me)'
 }
-
+import json
+import http
 
 print(Fore.MAGENTA + "Welcome to Unimc! Universal Minecraft Mod installer!", Fore.RESET)
 print(Back.CYAN + "Select your mode:" + Back.RESET)
@@ -15,4 +15,10 @@ print(Back.CYAN +
 1: Installing, here you can install mods from Modrinth!
 2: Remove .minecraft/mods folder (removes all mods)"""+Back.RESET)
 
-print(requests.get("https://api.modrinth.com/v2/query=fabric"))
+labrinthapi = requests.get("https://api.modrinth.com/v2/search?query=create",headers=headerss)
+labrinthapi.raise_for_status()
+response = labrinthapi.json()
+for key, value in response.items():
+    print(key, ":", value)
+#print(urllib.request.urlopen("https://api.modrinth.com/v2/search?query=create_mod"))
+#requestapi = urllib.request.urlopen("https://api.modrinth.com/v2/search?query=create_mod")
