828eb549817d510f027f12f20ac027bcd9d9dc07
Yehor <omelianenkoegor@gmail.com>
2023-09-30 17:42:04 +0200

    Now search is avaible!!

--- umimc.py
+++ umimc.py
@@ -22,12 +22,25 @@ debug_mode = True
 url_get_findrequest = "https://api.modrinth.com/v2/search?query="
 url_get_findrequest_string_type_mod = "&facets=[[%22project_type:mod%22]]"
 url_get_findproject = "https://api.modrinth.com/v2/project/"
+url_get_project = "https://api.modrinth.com/v2/project/"
+fmod_get_projectversion = "https://api.modrinth.com/v2/project/"
+fmod_get_projectversion2 = "/version"
+# Cdn links not important
+fmod_modrinth_cdn1 = "https://cdn.modrinth.com/data/"
+fmod_modrinth_cdn2 = "/versions/"
+# https://cdn.modrinth.com/data/AANobbMI/versions/mc1.16.3-0.1.0/sodium-fabric-mc1.16.3-0.1.0.jar
 # VERY IMPORTANT LISTS/VALUES/DICTIONARIES
-fmod_title = []
-fmod_projectid = []
-fmod_categories = []
+fmod_title = {}
+fmod_projectid = {}
+fmod_categories = {}
 fmod_dict = {}
+versions_dict = {}
 getmod_description = "None"
+
+# Stolen from stack overflow
+def prettysort(dct):
+    for item, amount in dct.items():  # dct.iteritems() in Python 2
+        print(Back.CYAN+"{} ({})".format(item, amount)+Back.RESET)
 
 # Printing Debug Mode Enabled
 if debug_mode == True:
@@ -81,8 +94,8 @@ if action == "1":
     response = labrinthapi.json()
 
     # Just text while waiting...
-    print(Back.LIGHTGREEN_EX + "Searching throught the response from Labrinth..." + Back.RESET)
-    
+    print(Fore.CYAN + "Searching throught the response from Labrinth..." + Fore.RESET)
+
     # Converting Labrinthapi response to json
     jlabrinthapi = labrinthapi.json()
     hits = jlabrinthapi["hits"]
@@ -91,9 +104,78 @@ if action == "1":
     # Searching entire response
     for mod in hits:
         mod_title = mod["title"]
-        mod_project_id = mod["project_id"]
-        fmod_dict[hits_count] = mod_title, mod_project_id
-    print(fmod_dict)
+        mod_id = mod["project_id"]
+        fmod_dict[hits_count] = mod_title
+        fmod_projectid[hits_count] = mod_id
+        hits_count += 1
+        if debug_mode == True:
+            print(debug_text+"Added "+mod_title+" to fmod_dict"+Fore.RESET)
+            print(debug_text+"It's id is: "+mod_id+Fore.RESET)
+        
+#    print(fmod_dict)
+
+    print(Back.CYAN + "Found "+ str(len(fmod_dict))+" search results"+Back.RESET)
+    prettysort(fmod_dict)
+    fmod_choose = int(input(Back.GREEN+"What to choose (type a number)?: "+Back.RESET))
+    fmod_chosen = 0
+    fmod_chosenid = 0
+    # Trying to get info from dictionaries
+    try:
+        fmod_chosen = fmod_dict[fmod_choose]
+        fmod_chosenid = fmod_projectid[fmod_choose]
+        if debug_mode == True:
+            print(debug_text + "Chosen mod is: "+fmod_chosen+"\n"+"Chosen mod project id is: "+fmod_chosenid+Fore.RESET)
+    except:
+        print(Back.RED+"Invalid argument!"+Back.RESET)
+    
+    # Getting cdn.modrinth download link
+    url_fmod_labrinthapi = fmod_get_projectversion+fmod_chosenid+fmod_get_projectversion2
+
+    fmod_labrinthapi = requests.get(url=fmod_get_projectversion+fmod_chosenid+fmod_get_projectversion2, headers=headerss, allow_redirects=True)
+
+    if debug_mode == True:
+        print(debug_text+"Requested labrinth at "+url_fmod_labrinthapi+Fore.RESET)
+    if fmod_labrinthapi.status_code == 200:
+        if debug_mode == True:
+            print(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
+    else:
+        print(Back.RED+str(fmod_labrinthapi.status_code)+Back.RESET)
+        print(Back.RED+"Something is wrong!"+Back.RESET)
+
+    # Converting to json
+    jfmod_labrinthapi = fmod_labrinthapi.json()
+
+    # Description for prettier look
+    fmod_desc_labrinthapi = requests.get(url="https://api.modrinth.com/v2/project/"+fmod_chosenid, headers=headerss, allow_redirects=True)
+    jfmod_desc_labrinthapi = fmod_desc_labrinthapi.json()
+    if debug_mode == True:
+        print(debug_text+"Requested labrinth at "+"https://api.modrinth.com/v2/project/"+fmod_chosenid+Fore.RESET)
+    if fmod_labrinthapi.status_code == 200:
+        if debug_mode == True:
+            print(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
+    else:
+        print(Back.RED+str(fmod_labrinthapi.status_code)+Back.RESET)
+        print(Back.RED+"Something is wrong!"+Back.RESET)
+    
+    # Printing description
+    print(Back.YELLOW+fmod_chosen)
+    print(jfmod_desc_labrinthapi["description"])
+    print("Followers: "+jfmod_desc_labrinthapi["followers"]+Back.RESET)
+
+    # Checking all of the versions
+    counterr = 0
+    just_versions_list_from_the_project = jfmod_desc_labrinthapi["versions"]
+    for bejba in just_versions_list_from_the_project:
+        try:
+            version_id = bejba[str(counterr)]
+            versions_dict[counterr] = version_id
+            if debug_mode == True:
+                print(debug_text+"Found version id: "+version_id+Fore.RESET)
+            counterr += 1
+        except:
+            print(versions_dict)
+
+
 
 elif action == "3":
     # Asking for Modrinth Project project id
