141561f70ad373354b89146c5ad6494324808cd3
Yehor <omelianenkoegor@gmail.com>
2023-10-17 14:01:15 +0200

    Early url search & updates

--- umimc.py
+++ umimc.py
@@ -18,7 +18,7 @@ headerss = {
     'User-Agent': 'yehorscode/Umimc (umimc@proton.me)'
 }
 # VERY IMPORT VALUES!
-debug_mode = False
+debug_mode = True
 url_get_findrequest = "https://api.modrinth.com/v2/search?query="
 url_get_findrequest_string_type_mod = "&facets=[[%22project_type:mod%22]]"
 url_get_findproject = "https://api.modrinth.com/v2/project/"
@@ -37,19 +37,24 @@ fmod_dict = {}
 versions_dict = {}
 getmod_description = "None"
 versions_id_dict = {}
-
+urllist = []
 # Stolen from stack overflow
 def prettysort(dct):
     for item, amount in dct.items():  # dct.iteritems() in Python 2
         print(Back.CYAN+"{} ({})".format(item, amount)+Back.RESET)
 
-# Printing Debug Mode Enabled
-if debug_mode == True:
-    print(Back.WHITE,Fore.BLACK,"Debug mode ENABLED",Fore.RESET,Back.RESET)
     
 
 # Defining what debug text looks like
 debug_text = Fore.GREEN + "DEBUG: "
+
+# Defining debug()
+def debug(debugtext):
+    if debug_mode == True:
+        print(f"{debug_text}{debugtext}{Fore.RESET}")
+
+# Printing Debug Mode Enabled
+debug(f"{Back.WHITE,Fore.BLACK}Debug mode ENABLED{Fore.RESET,Back.RESET}")
 
 # Welcome text
 print(Fore.GREEN + "Welcome to Umimc! Universal Minecraft Mod installer!\n", Fore.RESET)
@@ -72,8 +77,7 @@ action = input(Back.GREEN+"What do You choose (type here)?: "+Back.RESET)
 if action == "1":
 
     # Debug Mode Actions
-    if debug_mode == True:
-        print(debug_text+"Action 1 was choosed"+Fore.RESET)
+    debug(debug_text+"Action 1 was choosed"+Fore.RESET)
 
     # Choosing the mod
     request_modname = input(Back.GREEN+"What mod You want to Install?: "+ Back.RESET)
@@ -82,13 +86,11 @@ if action == "1":
     labrinthapi = requests.get(url_get_findrequest + request_modname + url_get_findrequest_string_type_mod , headers=headerss , allow_redirects=True)
 
     # Debbuging Responses
-    if debug_mode == True:
-        print(debug_text+"Trying to request Labrinth at: " + url_get_findrequest + request_modname + url_get_findrequest_string_type_mod, Fore.RESET)
+    debug(f"Trying to request Labrinth at: {url_get_findrequest + request_modname + url_get_findrequest_string_type_mod, Fore.RESET}")
 
     # Returning status code
     if labrinthapi.status_code == 200:
-        if debug_mode == True:
-            print(debug_text+ "Succesfully requested Labrinth")
+        debug(debug_text+ "Succesfully requested Labrinth")
     else:
         # Failed request and reason
         print(f"Request failed with status code: {labrinthapi.status_code}")
@@ -109,9 +111,8 @@ if action == "1":
         fmod_dict[hits_count] = mod_title
         fmod_projectid[hits_count] = mod_id
         hits_count += 1
-        if debug_mode == True:
-            print(debug_text+"Added "+mod_title+" to fmod_dict"+Fore.RESET)
-            print(debug_text+"It's id is: "+mod_id+Fore.RESET)
+        debug(debug_text+"Added "+mod_title+" to fmod_dict"+Fore.RESET)
+        debug(debug_text+"It's id is: "+mod_id+Fore.RESET)
         
 #    print(fmod_dict)
 
@@ -124,8 +125,7 @@ if action == "1":
     try:
         fmod_chosen = fmod_dict[fmod_choose]
         fmod_chosenid = fmod_projectid[fmod_choose]
-        if debug_mode == True:
-            print(debug_text + "Chosen mod is: "+fmod_chosen+"\n"+"Chosen mod project id is: "+fmod_chosenid+Fore.RESET)
+        debug(debug_text + "Chosen mod is: "+fmod_chosen+"\n"+"Chosen mod project id is: "+fmod_chosenid+Fore.RESET)
     except:
         print(Back.RED+"Invalid argument!"+Back.RESET)
     
@@ -134,11 +134,9 @@ if action == "1":
 
     fmod_labrinthapi = requests.get(url=fmod_get_projectversion+fmod_chosenid+fmod_get_projectversion2, headers=headerss, allow_redirects=True)
 
-    if debug_mode == True:
-        print(debug_text+"Requested labrinth at "+url_fmod_labrinthapi+Fore.RESET)
+    debug(debug_text+"Requested labrinth at "+url_fmod_labrinthapi+Fore.RESET)
     if fmod_labrinthapi.status_code == 200:
-        if debug_mode == True:
-            print(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
+        debug(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
     else:
         print(Back.RED+str(fmod_labrinthapi.status_code)+Back.RESET)
         print(Back.RED+"Something is wrong!"+Back.RESET)
@@ -149,11 +147,9 @@ if action == "1":
     # Description for prettier look
     fmod_desc_labrinthapi = requests.get(url="https://api.modrinth.com/v2/project/"+fmod_chosenid, headers=headerss, allow_redirects=True)
     jfmod_desc_labrinthapi = fmod_desc_labrinthapi.json()
-    if debug_mode == True:
-        print(debug_text+"Requested labrinth at "+"https://api.modrinth.com/v2/project/"+fmod_chosenid+Fore.RESET)
-    if fmod_labrinthapi.status_code == 200:
-        if debug_mode == True:
-            print(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
+    debug(debug_text+"Requested labrinth at "+"https://api.modrinth.com/v2/project/"+fmod_chosenid+Fore.RESET)
+    if fmod_desc_labrinthapi.status_code == 200:
+        debug(debug_text+"Succesfully accesed Labrinth Api!"+Fore.RESET)
     else:
         print(Back.RED+str(fmod_labrinthapi.status_code)+Back.RESET)
         print(Back.RED+"Something is wrong!"+Back.RESET)
@@ -176,9 +172,8 @@ if action == "1":
     for b in jfmod_desc_labrinthapi["versions"]:
         versions_id_dict[counterr] = b
         counterr +=1
-        if debug_mode == True:
-            print(debug_text+"Found "+str(len(versions_id_dict))+" versions id's"+Fore.RESET)
-            print(b)
+        debug(debug_text+"Found "+str(len(versions_id_dict))+" versions id's"+Fore.RESET)
+        debug(b)
     
     # User interaction with fmod (choosing mod id and mod name)
     fmod_user_interaction_modversion = input(Back.GREEN+"What version do You need?: "+Back.RESET)
@@ -188,9 +183,47 @@ if action == "1":
     for i in range(len(versions_dict)):
         if versions_dict[counterr] == fmod_user_interaction_modversion:
             print(Back.GREEN+"Found version!"+Back.RESET)
+            number_version = counterr
             break
         else:
             counterr+=1
+
+    # Choosing the download url
+    for counting_s_url in tqdm.tqdm(range(100)):
+        try:
+            number_s_url = jfmod_labrinthapi[counting_s_url]
+            files_s_url = number_s_url["files"]
+            files0_s_url = files_s_url[0]
+            for s_url in files0_s_url:
+                debug(f"Checking: {s_url}")
+                s_url_found = files0_s_url["url"]
+                debug(f"Checking: {s_url_found}")
+                urllist.append(s_url_found)
+        except IndexError:
+            debug("No more versions avaible (IndexError)")
+
+    # Printing urllist (debug)
+    for urllisti in range(len(urllist)):
+        debug(f"Found {urllist[urllisti]}")
+
+    # Showing how many there are url's
+    debug(f"There are {len(urllist)} avaible url's")
+    url_recheck_object = None
+    # Rechecking them
+    debug("Rechecking every of them for grammar/user error...")
+    for url_recheck in range(len(urllist)):
+        debug(f"Checking {url_recheck}")
+        if str(urllist).startswith("https://cdn.modrinth.com/data/") == True:
+            url_recheck_object = True
+        else:
+            url_recheck_object = False
+        debug(f"Obj number: {url_recheck} in urrlist is {url_recheck_object}")
+        debug(f"Obj name is {urllist[url_recheck]}")
+        
+
+    # Prompting the user
+#    for iteration in tqdm(range(len(urllist)))
+        
 
 elif action == "3":
     # Asking for Modrinth Project project id
@@ -201,8 +234,7 @@ elif action == "3":
     # Correct! ↓
     if len(project_id) == 8:
         print(Back.LIGHTCYAN_EX+"Project id is correct!"+Back.RESET)
-        if debug_mode == True:
-            print(debug_text+"Project_ID is: "+str(project_id)+Fore.RESET)
+        debug(debug_text+"Project_ID is: "+str(project_id)+Fore.RESET)
         labrinthapi_project_info = requests.get(url=url_get_findproject+project_id,headers=headerss,allow_redirects=True)
         print(labrinthapi_project_info)
 
@@ -217,8 +249,7 @@ elif action == "4":
     url_labrinthapi_statistics = "https://api.modrinth.com/v2/statistics"
     labrinthapi_statistics = requests.get(url_labrinthapi_statistics, allow_redirects=True,headers=headerss)
     if labrinthapi_statistics.status_code == 200:
-        if debug_mode == True:
-            print(debug_text+"Succesfully requested "+url_labrinthapi_statistics)
+        debug(debug_text+"Succesfully requested "+url_labrinthapi_statistics)
         print(Back.LIGHTMAGENTA_EX+"Labrinth is working succesfuly!\nHere you can check Modrinth's status: https://status.modrinth.com/"+Back.RESET)
     else:
         print(Back.LIGHTMAGENTA_EX+"Labrinth is NOT working succesfuly :( You are unable to install Mods!\nHere you can check Modrinth's status: https://status.modrinth.com/"+Back.RESET)
